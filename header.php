4a4412958872e1c9205a35839db927e1da3f631b
Pratikshatunge <89468480+Pratikshatunge@users.noreply.github.com>
2024-11-24 11:23:51 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,11 @@
+<section class="header">
+    <a href="Home.php" class="logo">wedding.</a>
+    <nav class="navbar">
+        <a href="Home.php">Home</a>
+        <a href="portfolio.php">Portfolio</a>
+        <a href="pricing.php">Pricing</a>
+        <a href="contact.php">Contact</a>
+        <a href="about.php">About</a>
+    </nav>
+    <div id="menu-btn" class="fas fa-bars"></div>
+</section>
