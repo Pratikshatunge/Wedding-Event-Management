464aa1d163d9a6c8aee889de4276ab2d04cb3629
Pratikshatunge <89468480+Pratikshatunge@users.noreply.github.com>
2024-11-24 11:24:25 +0530

    Add files via upload

---
+++ portfolio.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <?php include 'header.php'; ?>
+    <section class="portfolio">
+        <h1 class="heading">Our Portfolio</h1>
+        <div class="portfolio-container">
+
+            <a href="imgs/ceremony1.jpg" class="box">
+                <div class="image">
+                    <img src="imgs/ceremony1.jpg" alt="">
+                </div>
+                <h3>Wedding Ceremony</h3>
+            </a>
+
+            <a href="imgs/ceremony_2.jpg" class="box">
+                <div class="image">
+                    <img src="imgs/ceremony_2.jpg" alt="">
+                </div>
+                <h3>Wedding Ceremony</h3>
+            </a>
+
+            <a href="imgs/ceremony_3.jpg" class="box">
+                <div class="image">
+                    <img src="imgs/ceremony_3.jpg" alt="">
+                </div>
+                <h3>Wedding Ceremony</h3>
+            </a>
+
+            <a href="imgs/ceremony_4.jpg" class="box">
+                <div class="image">
+                    <img src="imgs/ceremony_4.jpg" alt="">
+                </div>
+                <h3>Wedding Ceremony</h3>
+            </a>
+
+            <a href="imgs/ceremony.jpg" class="box">
+                <div class="image">
+                    <img src="imgs/service-3.jpg" alt="">
+                </div>
+                <h3>Wedding Ceremony</h3>
+            </a>
+
+            <a href="imgs/ceremony_6.jpg" class="box">
+                <div class="image">
+                    <img src="imgs/ceremony_6.jpg" alt="">
+                </div>
+                <h3>Wedding Ceremony</h3>
+            </a>
+
+        </div>
+    </section>
+
+    <?php include 'footer.php'; ?>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+<script>
+    
+</script>
+
+</body>
+</html>
