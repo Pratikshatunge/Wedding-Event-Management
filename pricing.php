4926c7ec5550faa17acc29d1633cded347958207
Pratikshatunge <89468480+Pratikshatunge@users.noreply.github.com>
2024-11-24 11:24:44 +0530

    Add files via upload

---
+++ pricing.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pricing</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <?php include 'header.php'; ?>
+    <section class="Pricing">
+        <h1 class="heading">Our Pricing</h1>
+        <div class="box-container">
+            <div class="box">
+                <h3>Basic Plan</h3>
+                <div class="price">INR40000</div>
+                <div class="list">
+                    <p><i class="fas fa fa-check"></i>Photography</p>
+                    <p><i class="fas fa fa-check"></i>Bribe Makeup</p>
+                    <p><i class="fas fa fa-check"></i>Wedding Ceremony</p>
+                    <p><i class="fas fa fa-check"></i>meals & Drinks</p>
+                    <p><i class="fas fa fa-check"></i>Guest Invitations</p>
+                </div>
+                <a href="contact.php" class="btn">Choose Plan</a>
+            </div>
+
+            <div class="box">
+                <h3>Ultimate Plan</h3>
+                <div class="price">INR80000</div>
+                <div class="list">
+                    <p><i class="fas fa fa-check"></i>Photography</p>
+                    <p><i class="fas fa fa-check"></i>Bribe Makeup</p>
+                    <p><i class="fas fa fa-check"></i>Wedding Ceremony</p>
+                    <p><i class="fas fa fa-check"></i>meals & Drinks</p>
+                    <p><i class="fas fa fa-check"></i>Guest Invitations</p>
+                </div>
+                <a href="contact.php" class="btn">Choose Plan</a>
+            </div>
+
+            <div class="box">
+                <h3>Premium Plan</h3>
+                <div class="price">INR120000</div>
+                <div class="list">
+                    <p><i class="fas fa fa-check"></i>Photography</p>
+                    <p><i class="fas fa fa-check"></i>Bribe Makeup</p>
+                    <p><i class="fas fa fa-check"></i>Wedding Ceremony</p>
+                    <p><i class="fas fa fa-check"></i>meals & Drinks</p>
+                    <p><i class="fas fa fa-check"></i>Guest Invitations</p>
+                    <p><i class="fas fa fa-check"></i>Dj Service</p>
+                </div>
+                <a href="contact.php" class="btn">Choose Plan</a>
+            </div>
+        </div>
+    </section>
+    <section class="reviews">
+        <h1 class="heading">Happy Clients</h1>
+        <div class="reviews-slider">
+
+ <div class="w">
+
+ <div class="slide">
+ <img src="imgs/men_1.jpg" alt="">
+ <h3>Gajanan Shinde</h3>
+ <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    <div class="stars">
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+    </div>
+ </div>
+
+ <div class="slide">
+ <img src="imgs/women_1.jpg" alt="">
+ <h3>Gajanan Shinde</h3>
+ <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    <div class="stars">
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+    </div>
+ </div>
+
+ <div class="slide">
+ <img src="imgs/men_2.jpg" alt="">
+ <h3>Gajanan Shinde</h3>
+ <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    <div class="stars">
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+        <i class=" fas fa-star"></i>
+    </div>
+ </div>
+
+
+
+ </div>
+
+        </div>
+    </section>
+
+    <?php include 'footer.php'; ?>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
